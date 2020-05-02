5ab77f6341bcc248fa9780aaf457ded478f4aef6
MariusValentinDirvareanu <dirvareanumarius@live.com>
2020-05-02 18:07:21 +0300

    Implementare inregistrare neterminat

--- cMain.cpp
+++ cMain.cpp
@@ -3,50 +3,62 @@
 
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-	EVT_BUTTON(1001, OnButtonClicked)
+	EVT_BUTTON(1001, inregistrarePeroana)
+	EVT_BUTTON(1004, generareCNP)
 wxEND_EVENT_TABLE()
 
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Test", wxPoint(10, 10),wxSize(800, 600))
 {
+
+	wxSize s = wxSize(67, 20);
     
     mainPane = new wxPanel(this);
     
-	txt1 = new wxStaticText(mainPane, wxID_ANY, "CNP", wxPoint(170, 5), wxSize(60, 40));
-	m_txt1 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(170, 50), wxSize(100, 20));
+	txt1 = new wxStaticText(mainPane, wxID_ANY, "CNP", wxPoint(30, 30), s);
+	m_txt1 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 30), wxSize(300, 20));
 
-	txt2 = new wxStaticText(mainPane, wxID_ANY, "Nume", wxPoint(270, 5), wxSize(60, 40));
-	m_txt2 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(270, 50), wxSize(100, 20));
+	txt2 = new wxStaticText(mainPane, wxID_ANY, "Nume", wxPoint(30, 60), s);
+	m_txt2 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 60), wxSize(300, 20));
 
-	txt3 = new wxStaticText(mainPane, wxID_ANY, "Prenume", wxPoint(370, 5), wxSize(60, 40));
-	m_txt3 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(370, 50), wxSize(100, 20));
+	txt3 = new wxStaticText(mainPane, wxID_ANY, "Prenume", wxPoint(30, 90), s);
+	m_txt3 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 90), wxSize(300, 20));
 
-	txt4 = new wxStaticText(mainPane, wxID_ANY, "Serie", wxPoint(470, 5), wxSize(60, 40));
-	m_txt4 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(470, 50), wxSize(100, 20));
+	txt4 = new wxStaticText(mainPane, wxID_ANY, "Serie", wxPoint(500, 30), s);
+	m_txt4 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(600, 30), wxSize(100, 20));
 
-	txt5 = new wxStaticText(mainPane, wxID_ANY, "Numar", wxPoint(570, 5), wxSize(100, 40));
-	m_txt5 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt5 = new wxStaticText(mainPane, wxID_ANY, "Numar", wxPoint(500, 60), s);
+	m_txt5 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(600, 60), wxSize(100, 20));
 
-	txt6 = new wxStaticText(mainPane, wxID_ANY, "Cetatenie", wxPoint(570, 5), wxSize(100, 40));
-	m_txt6 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt6 = new wxStaticText(mainPane, wxID_ANY, "Cetatenie", wxPoint(30, 120), s);
+	m_txt6 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 120), wxSize(300, 20));
 
-	txt7 = new wxStaticText(mainPane, wxID_ANY, "Sex", wxPoint(570, 5), wxSize(100, 40));
-	m_txt7 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt7 = new wxStaticText(mainPane, wxID_ANY, "Sex", wxPoint(530, 120), s);
+	m_txt7 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(600, 120), wxSize(100, 20));
 
-	txt8 = new wxStaticText(mainPane, wxID_ANY, "LocNastere", wxPoint(570, 5), wxSize(100, 40));
-	m_txt8 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt8 = new wxStaticText(mainPane, wxID_ANY, "Loc Nastere", wxPoint(30, 150), s);
+	m_txt8 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 150), wxSize(300, 20));
 
-	txt9 = new wxStaticText(mainPane, wxID_ANY, "Domiciliu", wxPoint(570, 5), wxSize(100, 40));
-	m_txt9 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt9 = new wxStaticText(mainPane, wxID_ANY, "Domiciliu", wxPoint(30, 180), s);
+	m_txt9 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 180), wxSize(300, 20));
 
-	txt10 = new wxStaticText(mainPane, wxID_ANY, "EmisaDe", wxPoint(570, 5), wxSize(100, 40));
-	m_txt10 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt10 = new wxStaticText(mainPane, wxID_ANY, "Emisa de", wxPoint(30, 210), s);
+	m_txt10 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(100, 210), wxSize(300, 20));
 
-	txt11 = new wxStaticText(mainPane, wxID_ANY, "Valabilitate", wxPoint(570, 5), wxSize(100, 40));
-	m_txt11 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(570, 50), wxSize(100, 20));
+	txt11 = new wxStaticText(mainPane, wxID_ANY, "Valabilitate", wxPoint(500, 210), s);
+	m_txt11 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(450, 240), wxSize(200, 20));
+
+	txt12 = new wxStaticText(mainPane, wxID_ANY, "Data nasterii", wxPoint(500, 90), s);
+	m_txt12 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(600, 90), wxSize(40, 20));
+	m_txt13 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(650, 90), wxSize(40, 20));
+	m_txt14 = new wxTextCtrl(mainPane, wxID_ANY, "", wxPoint(700, 90), wxSize(40, 20));
 
 
-	m_btn1 = new wxButton(mainPane, 1001, "Click me!", wxPoint(10, 10), wxSize(150, 50));
+	m_btn1 = new wxButton(mainPane, 1001, "Inregistrare persoana", wxPoint(100, 300), wxSize(150, 50));
+	m_btn2 = new wxButton(mainPane, 1002, "Cautare CNP", wxPoint(300, 300), wxSize(150, 50));
+	m_btn3 = new wxButton(mainPane, 1003, "Stergere persoana", wxPoint(500, 300), wxSize(150, 50));
+	m_btn4 = new wxButton(mainPane, 1004, "Generare CNP", wxPoint(100, 400), wxSize(150, 50));
+	m_btn5 = new wxButton(mainPane, 1005, "Modificare date persoana", wxPoint(300, 400), wxSize(150, 50));
 
 
 	primaPornire();
@@ -128,47 +140,103 @@ void cMain::primaPornire() {
 
 
 
-void cMain::OnButtonClicked(wxCommandEvent& evt) {
-	exit = sqlite3_open(numeFisier.c_str(), &DB);
-	std::string ins;
-	if (m_txt1->GetValue() == wxT("") || m_txt2->GetValue() == wxT("") || m_txt5->GetValue() == wxT("")) {
-		wxMessageBox(wxT("Nu ai introdus destule valori"), wxT("Eroare"), wxICON_ERROR);
-	}
-	if (m_txt3->GetValue() != wxT("")) {
-		wxMessageBox(wxT("Nu poti avea si kg si litri"), wxT("Eroare"), wxICON_ERROR);
+void cMain::inregistrarePeroana(wxCommandEvent& evt) {
+	if (m_txt1->GetValue() == wxT("") || m_txt2->GetValue() == wxT("") || m_txt3->GetValue() == wxT("") || m_txt4->GetValue() == wxT("")
+		|| m_txt5->GetValue() == wxT("") || m_txt6->GetValue() == wxT("") || m_txt7->GetValue() == wxT("") || m_txt8->GetValue() == wxT("")
+		|| m_txt9->GetValue() == wxT("") || m_txt10->GetValue() == wxT("") || m_txt11->GetValue() == wxT("")) {
+
+		wxMessageBox(wxT("Nu ai introdus toate datele necesare!"), wxT("Eroare"), wxICON_ERROR);
 	}
 	else {
-		if (m_txt3->GetValue() != wxT("")) {
-			ins = "INSERT INTO stock(denumire,unitati,kg,pretunitate) VALUES('";
-			ins.append(m_txt1->GetValue() + "',");
-			ins.append(m_txt2->GetValue() + ",");
-			ins.append(m_txt3->GetValue() + ",");
-			ins.append(m_txt5->GetValue() + ");");
-			if (m_txt1->GetValue() != wxT("") && m_txt2->GetValue() != wxT("") && m_txt5->GetValue() != wxT("")) {
-				inserareDB(exit, ins);
-			}
-			else {
-				wxMessageBox(wxT("Valoare invalida"), wxT("Eroare"), wxICON_ERROR);
-			}
+		exit = sqlite3_open(numeFisier.c_str(), &DB);
+		std::string ins;
+
+
+		ins = "INSERT INTO buletin(CNP,Serie,Numar,Nume,Prenume,Cetatenie,Sex,LocNastere,Domiciliu,EmisaDe,Valabilitate) VALUES(";
+		ins.append(m_txt1->GetValue() + ",'");
+		ins.append(m_txt4->GetValue() + "',");
+		ins.append(m_txt5->GetValue() + ",'");
+		ins.append(m_txt2->GetValue() + "','");
+		ins.append(m_txt3->GetValue() + "','");
+		ins.append(m_txt6->GetValue() + "','");
+		ins.append(m_txt7->GetValue() + "','");
+		ins.append(m_txt8->GetValue() + "','");
+		ins.append(m_txt9->GetValue() + "','");
+		ins.append(m_txt10->GetValue() + "','");
+		ins.append(m_txt11->GetValue() + "');");
+
+		inserareDB(exit, ins);
+
+
+		sqlite3_close(DB);
+	}
+	evt.Skip();
+}
+
+void cMain::generareCNP(wxCommandEvent& evt)
+{
+	int s, aa, ll, zz, jj, nnn, c;
+	std::string cnp;
+	if (m_txt6->GetValue() == wxT("Romana")) {
+		if(m_txt7->GetValue() == wxT("M")){
+			s = 1;
 		}
 		else {
-			if (m_txt4->GetValue() != wxT("")) {
-				ins = "INSERT INTO stock(denumire,unitati,litri,pretunitate) VALUES('";
-				ins.append(m_txt1->GetValue() + "',");
-				ins.append(m_txt2->GetValue() + ",");
-				ins.append(m_txt4->GetValue() + ",");
-				ins.append(m_txt5->GetValue() + ");");
-				if (m_txt1->GetValue() != wxT("") && m_txt2->GetValue() != wxT("") && m_txt5->GetValue() != wxT("")) {
-					inserareDB(exit, ins);
-				}
-				else {
-					wxMessageBox(wxT("Valoare invalida"), wxT("Eroare"), wxICON_ERROR);
-				}
-			}
+			s = 2;
 		}
 	}
-	sqlite3_close(DB);
-	evt.Skip();
+	else {
+		if (m_txt7->GetValue() == wxT("M")) {
+			s = 7;
+		}
+		else {
+			s = 8;
+		}
+	}
+	aa = (std::stoi((std::string)m_txt14->GetValue())) % 100;
+	ll = std::stoi((std::string)m_txt13->GetValue());
+	zz = std::stoi((std::string)m_txt12->GetValue());
+	jj = 46;
+	nnn = rand() % 999 + 1;
+	c = (s / 2 + aa / 10 / 7 + aa % 10 / 9 + ll / 10 / 1 + ll % 10 / 4 + zz / 10 / 6 + zz % 10 / 3 + jj / 10 / 5 + jj % 10 / 8 + nnn / 100 / 5 + nnn / 10 % 10 / 7 + nnn % 10 / 9) % 11;
+	cnp.append(std::to_string(s));
+	if (aa < 10) {
+		cnp.append("0" + std::to_string(aa));
+	}
+	else
+	{
+		cnp.append(std::to_string(aa));
+	}
+	
+	if (ll < 10) {
+		cnp.append("0" + std::to_string(ll));
+	}
+	else
+	{
+		cnp.append(std::to_string(ll));
+	}
+	if (zz < 10) {
+		cnp.append("0" + std::to_string(zz));
+	}
+	else {
+		cnp.append(std::to_string(zz));
+	}
+	
+	cnp.append(std::to_string(jj));
+	if (nnn < 10) {
+		cnp.append("00" + std::to_string(nnn));
+	}
+	else {
+		if (nnn < 100) {
+			cnp.append("0" + std::to_string(nnn));
+		}
+		else
+		{
+			cnp.append(std::to_string(nnn));
+		}
+	}
+	cnp.append(std::to_string(c));
+	m_txt1->AppendText(cnp);
 }
 
 cMain::~cMain()
--- cMain.h
+++ cMain.h
@@ -4,6 +4,7 @@
 #include "wx/listctrl.h"
 #include "sqlite3.h"
 #include <vector>
+#include <map>
 #include <string>
 #include <filesystem>
 #include "wx/msgdlg.h"
@@ -19,16 +20,35 @@ public:
 	void inserareDB(int& exit, std::string& queryInserare);
 	void stergereDB(int& exit, std::string& queryDelete);
 	void primaPornire();
-	void OnButtonClicked(wxCommandEvent& evt);
+	void inregistrarePeroana(wxCommandEvent& evt);
+	void generareCNP(wxCommandEvent& evt);
+	void cautarePersoana(wxCommandEvent& evt);
+	void stergerePersoana(wxCommandEvent& evt);
+	void modificarePersoana(wxCommandEvent& evt);
 
 public:
 	wxButton* m_btn1 = nullptr;
 	wxButton* m_btn2 = nullptr;
 	wxButton* m_btn3 = nullptr;
+	wxButton* m_btn4 = nullptr;
+	wxButton* m_btn5 = nullptr;
 	wxPanel* mainPane = nullptr;
 	sqlite3* DB;
 	unsigned ok = 0;
 	std::string numeFisier = "persoane.db";
+	std::map<int, std::vector<std::string>> judete = { {1,{"Alba","AB"}},{2,{"Arad","AR"}},{3,{"Arges","AG"}},
+		{4,{"Bacau","BC"}},{5,{"Bihor","BH"}},{6,{"Bistrita-Nasaud","BN"}},{7,{"Botosani","BT"}},
+		{8,{"Brasov","BV"}},{9,{"Braila","BR"}},{10,{"Buzau","BZ"}},{11,{"Caras-Severin","CS"}},
+		{12,{"Cluj","CJ"}},{13,{"Constanta","CT"}},{14,{"Covasna","CV"}},{15,{"Dambovita","DB"}},
+		{16,{"Dolj","DJ"}},{17,{"Galati","GL"}},{18,{"Gorj","GJ"}},{19,{"Harghita","HR"}},
+		{20,{"Hunedoara","HD"}},{21,{"Ialomita","IL"}},{22,{"Iasi","IS"}},{23,{"Ilfov","IF"}},
+		{24,{"Maramures","MM"}},{25,{"Mehedinti","MH"}},{26,{"Mures","MS"}},{27,{"Neamt","NT"}},
+		{28,{"Olt","OT"}},{29,{"Prahova","PH"}},{30,{"Satu Mare","SM"}},{31,{"Salaj","SJ"}},
+		{32,{"Sibiu","SB"}},{33,{"Suceava","SV"}},{34,{"Teleorman","TR"}},{35,{"Timis","TM"}},
+		{36,{"Tulcea","TL"}},{37,{"Vaslui","VS"}},{38,{"Valcea","VL"}},{39,{"Vrancea","VN"}},
+		{40,{"Bucuresti","B"}},{41,{"Bucuresti - Sector 1","B"}},{42,{"Bucuresti - Sector 2","B"}},
+		{43,{"Bucuresti - Sector 3","B"}},{44,{"Bucuresti - Sector 4","B"}},{45,{"Bucuresti - Sector 5","B"}},
+		{46,{"Bucuresti - Sector 6","B"}},{51,{"Calarasi","CL"}},{52,{"Giurgiu","GR"}}, };
 	int exit = 0;
 	char* messaggeError;
 	wxStaticText* txt1 = nullptr;
@@ -53,6 +73,10 @@ public:
 	wxTextCtrl* m_txt10 = nullptr;
 	wxStaticText* txt11 = nullptr;
 	wxTextCtrl* m_txt11 = nullptr;
+	wxStaticText* txt12 = nullptr;
+	wxTextCtrl* m_txt12 = nullptr;
+	wxTextCtrl* m_txt13 = nullptr;
+	wxTextCtrl* m_txt14 = nullptr;
 
 	wxDECLARE_EVENT_TABLE();
 };
