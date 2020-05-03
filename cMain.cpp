a680c01b96a98ab6bf60cca753565562bff44f19
MariusValentinDirvareanu <dirvareanumarius@live.com>
2020-05-03 16:02:59 +0300

    END

--- cMain.cpp
+++ cMain.cpp
@@ -106,7 +106,6 @@ void cMain::inserareDB(int& exit, std::string& queryInserare)
 		sqlite3_free(messaggeError);
 	}
 	else {
-		std::cout << "Records created Successfully!" << std::endl;
 		wxMessageBox(wxT("Inregistrarea s-a facut cu succes!"), wxT("Succes!"), wxICON_INFORMATION);
 	}
 }
